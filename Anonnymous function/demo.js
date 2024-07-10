5eb79901759f9f9ddcd5edbe8b85e0db4c112cb6
JAYA SURYA <133043397+Jayasurya-rookie@users.noreply.github.com>
2024-07-10 21:02:26 +0530

    Add files via upload

---
+++ Anonnymous function/demo.js
@@ -0,0 +1,43 @@
+ function normal (){
+    console.log("normal function");
+ }
+
+ normal();
+
+ let a =function(){
+    console.log("ananymous function");
+    return 6;
+ }
+ console.log(a());
+
+ let x =()=>6;
+let z=()=>{
+    console.log("second arraw function");
+    return 2;
+}
+console.log(x());
+console.log(z());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  let x=function(){
+// console.log("surya");
+// return 5;
+// }
+// console.log(x());
